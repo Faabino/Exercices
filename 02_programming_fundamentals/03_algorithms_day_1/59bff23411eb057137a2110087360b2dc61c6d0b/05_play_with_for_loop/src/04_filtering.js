aef87e6a110a3520a2b8d2a8c817b6cb4a92755b
Fabien <faabino6z@live.fr>
2020-09-12 23:44:18 +0200

    Loop ex done

--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/04_filtering.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/04_filtering.js
@@ -2,3 +2,18 @@
 //
 // -  Create an array called `literalDigits` from `zero` to `nine` where each array entry is a spelled-out number;
 // -  Using `length`, write on `stdout` each odd values of the table.
+const literalDigits = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine"];
+
+for (let i = 1; i < literalDigits.length; i++) {
+  console.log(literalDigits.filter(2 % 0 === 0));
+}
--- 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/09_filtering_with_while.js
+++ 02_programming_fundamentals/03_algorithms_day_1/59bff23411eb057137a2110087360b2dc61c6d0b/05_play_with_for_loop/src/09_filtering_with_while.js
@@ -2,3 +2,4 @@
 //
 // -  Create an array called `litteralDigits` from `zero` to `nine` where each array entry is a spelled-out number;
 // -  Using `length`, write on `stdout` each odd values of the table.
+const litteralDigits = [];
