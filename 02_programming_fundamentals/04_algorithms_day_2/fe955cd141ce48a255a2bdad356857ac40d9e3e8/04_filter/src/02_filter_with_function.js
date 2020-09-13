0fb240a3ff1de7f78388d14291090f8369b62b92
Fabien <faabino6z@live.fr>
2020-09-13 20:56:36 +0200

    ex filter with fn done

--- 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/04_filter/src/02_filter_with_function.js
+++ 02_programming_fundamentals/04_algorithms_day_2/fe955cd141ce48a255a2bdad356857ac40d9e3e8/04_filter/src/02_filter_with_function.js
@@ -1,6 +1,28 @@
-function filter(array, fn) {
-  // Your code here
+function isOdd(num) {
+  if (num % 2 !== 0) {
+    return true;
+  }
+  return false;
 }
+
+function isEven(num) {
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
+}
+
+function filter(array, fn) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (fn(array[i])) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
+
+console.log(filter([1, 2, 3, 4, 5], isOdd));
 
 // do not remove this line, it is for tests
 module.exports = filter;
