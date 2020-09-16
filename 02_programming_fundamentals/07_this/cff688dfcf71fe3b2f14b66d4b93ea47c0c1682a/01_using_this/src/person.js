a05c57097aa111e6d401563e74ab28a491219b2c
Fabien <faabino6z@live.fr>
2020-09-16 10:14:13 +0200

    This, first ex done

---
+++ 02_programming_fundamentals/07_this/cff688dfcf71fe3b2f14b66d4b93ea47c0c1682a/01_using_this/src/person.js
@@ -0,0 +1,17 @@
+const person = {
+  firstname: "Abdel",
+  lastname: "Sadki",
+  age: 42,
+  fullname: function () {
+    return `${this.firstname} ${this.lastname}`;
+  },
+  // Complete here
+  introduceMyself: function () {
+    return `Hello! I'm ${this.firstname} ${this.lastname} and I'm ${this.age}.`;
+  },
+};
+
+const introduction = person.introduceMyself();
+console.log(introduction);
+
+module.exports = person;
