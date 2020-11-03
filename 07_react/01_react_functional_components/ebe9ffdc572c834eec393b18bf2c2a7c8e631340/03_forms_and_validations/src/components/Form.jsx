c19507e6fa56c7825e6c35cb1fdeef8961c3e13d
Fabien <faabino6z@live.fr>
2020-11-03 12:13:51 +0100

    React form validation exo done

--- 07_react/01_react_functional_components/ebe9ffdc572c834eec393b18bf2c2a7c8e631340/03_forms_and_validations/src/components/Form.jsx
+++ 07_react/01_react_functional_components/ebe9ffdc572c834eec393b18bf2c2a7c8e631340/03_forms_and_validations/src/components/Form.jsx
@@ -1,5 +1,48 @@
 import React from "react";
 
-const Form = () => "Code here";
+const Form = () => {
+  const [inputClassName, setInputClassName] = React.useState("form-control");
+  const [currentValue, setCurrentValue] = React.useState("");
+
+  const checkInput = (event) => {
+    setCurrentValue(event.target.value);
+
+    /\s/g.test(event.target.value)
+      ? setInputClassName("form-control is-invalid")
+      : setInputClassName("form-control");
+  };
+
+  const handleSubmit = (event) => {
+    alert("A name was submitted: " + currentValue);
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>First Name</label>
+          <input
+            type="text"
+            className={inputClassName}
+            id="firstName"
+            aria-describedby="validationFeedback"
+            value={currentValue}
+            onChange={checkInput}
+            required
+          />
+          {inputClassName === "form-control is-invalid" && (
+            <div id="validationFeedback" className="invalid-feedback">
+              No spaces allowed
+            </div>
+          )}
+        </div>
+        <button type="submit" className="btn btn-primary" value="Submit">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
 
 export default Form;
