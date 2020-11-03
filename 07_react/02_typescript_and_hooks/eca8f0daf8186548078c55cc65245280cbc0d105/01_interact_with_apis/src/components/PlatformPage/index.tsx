2dd4a602055776a6998545b7be95aa18cc0111fb
Fabien <faabino6z@live.fr>
2020-11-03 18:20:51 +0100

    React API day2 in progress

--- 07_react/02_typescript_and_hooks/eca8f0daf8186548078c55cc65245280cbc0d105/01_interact_with_apis/src/components/PlatformPage/index.tsx
+++ 07_react/02_typescript_and_hooks/eca8f0daf8186548078c55cc65245280cbc0d105/01_interact_with_apis/src/components/PlatformPage/index.tsx
@@ -2,24 +2,24 @@ import React from "react";
 import axios from "axios";
 
 const PlatformPage = (): JSX.Element => {
+  const [platforms, setPlatforms] = React.useState();
+
   React.useEffect(() => {
-    axios("https://videogames-sparta.herokuapp.com/", {
-      headers: new Headers({
+    axios("https://videogames-sparta.herokuapp.com/platforms", {
+      headers: {
         Accept: "application/json",
-      }),
+      },
     })
       .then((response) => {
-        console.log(response);
+        setPlatforms(response.data);
       })
       .catch((error) => {
         console.error(error);
-      });
-    return (): void => {
-      console.log("Unmounted");
-    };
-  }, []);
+      }),
+      [setPlatforms];
+  });
 
-  return <>Platforms</>;
+  return <div>{JSON.stringify(platforms)};</div>;
 };
 
 export default PlatformPage;
