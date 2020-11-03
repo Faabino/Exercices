dd982a1f0c274c5d4325642fd993d48a32555e25
Fabien <faabino6z@live.fr>
2020-11-03 16:29:11 +0100

    day 2 react

--- 07_react/02_typescript_and_hooks/eca8f0daf8186548078c55cc65245280cbc0d105/01_interact_with_apis/src/components/PlatformPage/index.tsx
+++ 07_react/02_typescript_and_hooks/eca8f0daf8186548078c55cc65245280cbc0d105/01_interact_with_apis/src/components/PlatformPage/index.tsx
@@ -1,6 +1,24 @@
 import React from "react";
+import axios from "axios";
 
 const PlatformPage = (): JSX.Element => {
+  React.useEffect(() => {
+    axios("https://videogames-sparta.herokuapp.com/", {
+      headers: new Headers({
+        Accept: "application/json",
+      }),
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    return (): void => {
+      console.log("Unmounted");
+    };
+  }, []);
+
   return <>Platforms</>;
 };
 
